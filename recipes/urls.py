cb242603c0f7cc5b9f51df74d5137bd752ffacdd
Peter <peter71302@gmail.com>
2018-02-12 18:45:00 +0300

    adding recipes pages

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from . import views
+from recipes import views
+from recipes.models import *
 
 urlpatterns = [
-	url(r'^recipes/', views.recipes, name = 'recipes'),
+	url(r'^recipes/(?P<recipe_id>\w+)/$', views.recipe, name = 'recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from recipes.models import *
 
-def recipes(request):
+def recipe(request, recipe_id):
     
+    recipe = Recipe.objects.get(id=recipe_id)
     return render(request, 'recipes/recipes.html')
