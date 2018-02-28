c152d9ca362219e0d2017a07155cde8f0cd1d9af
Peter <peter71302@gmail.com>
2018-02-28 19:24:24 +0300

    updated views_counter with cookies and a few little things added

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from recipes.models import *
 from django.db.models import Count
+from django.http import HttpResponseRedirect
 
 def recipe(request, recipe_id):
     
     recipe = Recipe.objects.get(id=recipe_id)
-    recipe.views_counter += 1
-    recipe.save(update_fields=['views_counter'])
     recipes = Recipe.objects.order_by('-views_counter')
     k = 0
     while k < 2:
@@ -22,5 +21,11 @@ def recipe(request, recipe_id):
         recipe_best.status = 'Свежее'
         recipe_best.save(update_fields=['status'])
         k += 1
+    if recipe_id not in request.COOKIES:
+        recipe.views_counter += 1
+        recipe.save(update_fields=['views_counter'])
+        response = render(request, 'recipes/recipes.html', locals())
+        response.set_cookie(recipe_id, 'already seen')
+        return response
         
     return render(request, 'recipes/recipes.html', locals())
