06b2431cc8582f5cb27a38831b4e42998b970588
K0mp0t <peter71302@gmail.com>
2018-03-24 19:27:18 +0300

    Update settings.py

--- kmpt/settings.py
+++ kmpt/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 STATIC_URL = '/static/'
 STATIC_ROOT = ''
 MEDIA_URL = '/media/'
