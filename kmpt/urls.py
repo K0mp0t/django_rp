3674de965e747aaf00d26bdce777de7d24fd21bf
K0mp0t <peter71302@gmail.com>
2018-03-24 19:25:16 +0300

    Update urls.py

--- kmpt/urls.py
+++ kmpt/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
 	url(r'^', include('landing.urls')),
     url(r'^', include('recipes.urls')),
     url(r'^', include('search.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
          
