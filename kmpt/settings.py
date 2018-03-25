2b9abfe83ae7ec5a52a0eb828e216c1c1258729b
k0mp0t <peter71302@gmail.com>
2018-03-25 13:30:55 +0000

    do commit

--- kmpt/settings.py
+++ kmpt/settings.py
@@ -129,7 +129,7 @@ STATICFILES_FINDERS = (
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR + '/media/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),
