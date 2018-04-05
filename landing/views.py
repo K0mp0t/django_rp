380189c9e8d64c09953849324b252f68f74fa354
K0mp0t <peter71302@gmail.com>
2018-04-05 17:38:33 +0300

    Update views.py

--- landing/views.py
+++ landing/views.py
@@ -29,33 +29,11 @@ def home(request):
 	
 def new(request):
 	
-	# recipes = Recipe.objects.all()
-	# for recipe in recipes:
-	# 	recipe.status = 'Архив'
-	# 	recipe.save(update_fields=['status'])
-	# k = 0
-	# recipes = Recipe.objects.order_by('created')
-	# for i in range(2):
-	# 	recipe_new = recipes[0+k]
-	# 	recipe_new.status = 'Свежее'
-	# 	recipe_new.save(update_fields=['status'])
-	# 	k += 1
 	recipes_new = Recipe.objects.filter(status='Свежее')
 	return render(request, 'recipes/new.html', locals())
 
 def best(request):
 	
-	# recipes = Recipe.objects.all()
-	# for recipe in recipes:
-	# 	recipe.status = 'Архив'
-	# 	recipe.save(update_fields=['status'])
-	# recipes = Recipe.objects.order_by('-views_counter')
-	# k = 0
-	# for i in range(1):
-	# 	recipe_best = recipes[0+k]
-	# 	recipe_best.status = 'Лучшее'
-	# 	recipe_best.save(update_fields=['status'])
-	# 	k += 1
 	recipes_best = Recipe.objects.filter(status='Лучшее')
 	return render(request, 'recipes/best.html', locals())
 
